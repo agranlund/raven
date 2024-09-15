bed9a4e002aa3165afa773b721bdef5c1b680bb1
Anders Granlund <granlund23@yahoo.se>
2024-09-15 15:45:30 +0200

    mon: raven.h purec compatibility

--- sw/rom/mon/raven.h
+++ sw/rom/mon/raven.h
@@ -7,29 +7,39 @@ Important:
 #ifndef _RAVEN_H_
 #define _RAVEN_H_
 
+/*--------------------------------------------------------------------------------*/
+#define C_RAVN_VER      0x20240913UL
+#define C_RAVN_PTR      0x40000000UL
 #ifndef C_RAVN
-#define C_RAVN          0x5241564E
+#define C_RAVN          0x5241564EUL
 #endif
 
+/*--------------------------------------------------------------------------------*/
 #if defined(__GNUC__)
 #define _RVAPI
+#define _RVINL static inline
 #else
-#define _RVAPI  cdecl
+#define _RVAPI cdecl
+#define _RVINL static
 #endif
 
+/*--------------------------------------------------------------------------------*/
 typedef struct
 {
 /* 0x0000 */
     uint32_t    magic;
     uint32_t    version;
     uint32_t    reserved0000[6];
+
 /* 0x0020 */
     void        _RVAPI (*dbg_GPO)(int32_t num, int32_t val);
     uint32_t    reserved0020[7];
+
 /* 0x0040 */
     void        _RVAPI (*rtc_Get)(uint32_t addr, uint8_t* buf, uint32_t siz);
     void        _RVAPI (*rtc_Set)(uint32_t addr, uint8_t* buf, uint32_t siz);
     uint32_t    reserved0040[6];
+
 /* 0x0060 */
     int32_t     _RVAPI (*i2c_Aquire)(void);
     void        _RVAPI (*i2c_Release)(void);
@@ -38,10 +48,11 @@ typedef struct
     uint32_t    _RVAPI (*i2c_Read)(uint32_t ack);
     uint32_t    _RVAPI (*i2c_Write)(uint32_t val);
     uint32_t    reserved0060[2];
+    
 /* 0x0080 */
 
 } raven_t;
 
-#endif // _RAVEN_H_
+#endif /* _RAVEN_H_ */
 
 
