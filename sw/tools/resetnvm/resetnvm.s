c1c5e0991610945ae1fe12dca10375dd0b89c1bc
Anders Granlund <granlund23@yahoo.se>
2024-10-27 13:58:11 +0100

    import resetnvm source

---
+++ sw/tools/resetnvm/resetnvm.s
@@ -0,0 +1,56 @@
+;-------------------------------------------------------------------------------
+; resetnvm
+; (c) 2024 Anders Granlund
+;
+; resets the Atari compatible portion of nvram
+; inspired by CT60 tool with same name
+;-------------------------------------------------------------------------------
+;
+; This file is free software  you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation  either version 2, or (at your option)
+; any later version.
+;
+; This file is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY  without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program  if not, write to the Free Software
+; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+;
+;-------------------------------------------------------------------------------
+
+	BSS
+gstackb:	ds.l	256
+gstackt:	ds.l	4
+
+	TEXT
+init:
+	move.l	4(sp),a0
+	lea		gstackt(pc),sp
+	move.l	#0x1100,d0
+	add.l	0x0c(a0),d0
+	add.l	0x14(a0),d0
+	add.l	0x1c(a0),d0
+
+	move.l	d0,-(sp)			; Mshrink
+	move.l	a0,-(sp)
+	clr.w	-(sp)
+	move.w	#0x4a,-(sp)
+	trap	#1
+	add.l	#12,sp
+
+main:
+	clr.l	-(sp)				; Nvmaccess
+	clr.w	-(sp)
+	clr.w	-(sp)
+	move.w	#2,-(sp)
+	move.w	#46,-(sp)
+	trap	#14
+	lea.l	12(sp),sp
+
+done:
+	clr.w	-(sp)				; Pterm0
+	trap	#1
