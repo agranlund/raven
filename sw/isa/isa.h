7060eeb1bfc104843f6c8fe9a5d0583520940316
Anders Granlund <granlund23@yahoo.se>
2024-12-25 17:15:53 +0100

    isa.h purec fix

--- sw/isa/isa.h
+++ sw/isa/isa.h
@@ -1,11 +1,18 @@
 #ifndef _ISA_H_
 #define _ISA_H_
 
-//----------------------------------------------------------------------
-// Constants
-//----------------------------------------------------------------------
-#define C__ISA  0x5F495341          /* '_ISA' */
-#define C__PNP  0x5F504E50          /* '_PNP' */
+#include <stddef.h>
+#include <stdint.h>
+
+/*----------------------------------------------------------------------
+ * Constants
+ *--------------------------------------------------------------------*/
+#ifndef C__ISA
+#define C__ISA  0x5F495341UL        /* '_ISA' */
+#endif
+#ifndef C__PNP
+#define C__PNP  0x5F504E50UL        /* '_PNP' */
+#endif
 
 #define ISA_ENDIAN_BE       0       /* Big endian                       */
 #define ISA_ENDIAN_LEAS     1       /* Little endian: Address swapped   */
@@ -21,50 +28,68 @@
 #define ISA_MAX_DEV_IRQ     2
 #define ISA_MAX_DEV_DMA     2
 
-//----------------------------------------------------------------------
-// Device
-//----------------------------------------------------------------------
+/*----------------------------------------------------------------------
+ * Device
+ *--------------------------------------------------------------------*/
 typedef struct
 {
-    unsigned int    id[ISA_MAX_DEV_IDS];
-    unsigned int    mem[ISA_MAX_DEV_MEM];
-    unsigned short  port[ISA_MAX_DEV_PORT];
-    unsigned char   irq[ISA_MAX_DEV_IRQ];
-    unsigned char   dma[ISA_MAX_DEV_DMA];
+    uint32_t    id[ISA_MAX_DEV_IDS];
+    uint32_t    mem[ISA_MAX_DEV_MEM];
+    uint16_t    port[ISA_MAX_DEV_PORT];
+    uint8_t     irq[ISA_MAX_DEV_IRQ];
+    uint8_t     dma[ISA_MAX_DEV_DMA];
 } isa_dev_t;
 
 
-//----------------------------------------------------------------------
-// Bus interface
-//----------------------------------------------------------------------
+/*----------------------------------------------------------------------
+ * Bus interface
+ * GCC and PureC compatible
+ *--------------------------------------------------------------------*/
+
+#ifdef __GNUC__
+    typedef unsigned char   uint8l_t;
+    typedef unsigned short  uint16l_t;
+    #define _ISA_API
+#else
+    typedef unsigned long   uint8l_t;
+    typedef unsigned long   uint16l_t;
+    #define _ISA_API        cdecl
+#endif
+
 typedef struct
 {
-    unsigned short  version;
-    unsigned int    iobase;
-    unsigned int    membase;
-    unsigned short  irqmask;
-    unsigned char   drqmask;
-    unsigned char   endian;
+    uint16_t    version;
+    uint32_t    iobase;
+    uint32_t    membase;
+    uint16_t    irqmask;
+    uint8_t     drqmask;
+    uint8_t     endian;
 
-    void            (*outp)(unsigned short port, unsigned char data);
-    void            (*outpw)(unsigned short port, unsigned short data);
-    unsigned char   (*inp)(unsigned short port);
-    unsigned short  (*inpw)(unsigned short addr);
+    void        _ISA_API (*outp)(uint16l_t port, uint8l_t data);
+    void        _ISA_API (*outpw)(uint16l_t port, uint16l_t data);
+    uint8_t     _ISA_API (*inp)(uint16l_t port);
+    uint16_t    _ISA_API (*inpw)(uint16l_t addr);
 
-    void            (*outp_buf)(unsigned short port, unsigned char* buf, int count);
-    void            (*outpw_buf)(unsigned short port, unsigned short* buf, int count);
-    void            (*inp_buf)(unsigned short port, unsigned char* buf, int count);
-    void            (*inpw_buf)(unsigned short port, unsigned short* buf, int count);
+    void        _ISA_API (*outp_buf)(uint16l_t port, uint8_t* buf, int32_t count);
+    void        _ISA_API (*outpw_buf)(uint16l_t port, uint16_t* buf, int32_t count);
+    void        _ISA_API (*inp_buf)(uint16l_t port, uint8_t* buf, int32_t count);
+    void        _ISA_API (*inpw_buf)(uint16l_t port, uint16_t* buf, int32_t count);
 
-    unsigned int    (*irq_set)(unsigned char irq, unsigned int func);
-    unsigned int    (*irq_en)(unsigned char irq, unsigned char enabled);
+    uint32_t    _ISA_API (*irq_set)(uint8l_t irq, uint32_t func);
+    uint32_t    _ISA_API (*irq_en)(uint8l_t irq, uint8l_t enabled);
 
-    isa_dev_t*      (*find_dev)(const char* id, unsigned short idx);
+    isa_dev_t*  _ISA_API (*find_dev)(const char* id, uint16l_t idx);
 
-    unsigned short  numdevs;
-    isa_dev_t       devs[ISA_MAX_DEVS];
+#if 0
+    void        _ISA_API (*delayus)(uint32_t us);
+    void        _ISA_API (*delayms)(uint32_t ms);
+#endif
+
+    uint16_t    numdevs;
+    isa_dev_t   devs[ISA_MAX_DEVS];
 
 } isa_t;
 
 
-#endif // _ISA_H_
+#endif /* _ISA_H_ */
+
