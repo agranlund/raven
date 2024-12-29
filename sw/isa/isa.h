b3600c159c836f49ac9e6dacceda27e608017822
Anders Granlund <granlund23@yahoo.se>
2024-12-29 19:58:38 +0100

    Adding minimal header-only library to isa.h

--- sw/isa/isa.h
+++ sw/isa/isa.h
@@ -1,11 +1,39 @@
+/*----------------------------------------------------------------------
+ *
+ * Atari ISA bus header only library
+ * 
+ * todo: fallback interface if isa_bios is not installed
+ * 
+ *--------------------------------------------------------------------*/
 #ifndef _ISA_H_
 #define _ISA_H_
 
 #include <stddef.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 /*----------------------------------------------------------------------
- * Constants
+ *
+ * GCC / PureC compatibility
+ * 
+ *--------------------------------------------------------------------*/
+#ifdef __GNUC__
+    typedef unsigned char   uint8l_t;
+    typedef unsigned short  uint16l_t;
+    #define _ISA_API
+    #define _ISA_INL static inline
+#else
+    typedef unsigned long   uint8l_t;
+    typedef unsigned long   uint16l_t;
+    #define _ISA_API cdecl
+    #define _ISA_INL static
+#endif
+
+
+/*----------------------------------------------------------------------
+ *
+ * ISA_BIOS cookie interface
+ * 
  *--------------------------------------------------------------------*/
 #ifndef C__ISA
 #define C__ISA  0x5F495341UL        /* '_ISA' */
@@ -28,9 +56,6 @@
 #define ISA_MAX_DEV_IRQ     2
 #define ISA_MAX_DEV_DMA     2
 
-/*----------------------------------------------------------------------
- * Device
- *--------------------------------------------------------------------*/
 typedef struct
 {
     uint32_t    id[ISA_MAX_DEV_IDS];
@@ -40,21 +65,6 @@ typedef struct
     uint8_t     dma[ISA_MAX_DEV_DMA];
 } isa_dev_t;
 
-
-/*----------------------------------------------------------------------
- * Bus interface
- * GCC and PureC compatible
- *--------------------------------------------------------------------*/
-
-#ifdef __GNUC__
-    typedef unsigned char   uint8l_t;
-    typedef unsigned short  uint16l_t;
-    #define _ISA_API
-#else
-    typedef unsigned long   uint8l_t;
-    typedef unsigned long   uint16l_t;
-    #define _ISA_API        cdecl
-#endif
 
 typedef struct
 {
@@ -91,5 +101,59 @@ typedef struct
 } isa_t;
 
 
-#endif /* _ISA_H_ */
+/*----------------------------------------------------------------------
+ *
+ * Small header-only helper library
+ * 
+ *--------------------------------------------------------------------*/
+#ifndef ISA_EXCLUDE_LIBRARY
+#include <mint/cookie.h>
 
+static isa_t* isa_if = NULL;
+static bool isa_inited = false;
+
+_ISA_INL isa_t* isa_init_fallback(void) {
+    /* here we could init a fallback isa_t interface for known computers
+     * that does now have isa_bios installed */
+    return NULL;
+}
+
+_ISA_INL isa_t* isa_init(void) {
+    uint32_t cookie;
+    if (!isa_inited) {
+        isa_inited = true;
+        if (Getcookie(C__ISA, (long*)&cookie) == C_FOUND) {
+            isa_if = (isa_t*)cookie;
+        } else {
+            isa_if = isa_init_fallback();
+        }
+    }
+    return isa_if;
+}
+
+_ISA_INL void isa_delay(uint32_t micros) {
+    (void)micros;
+    /* todo */
+}
+
+_ISA_INL void isa_writeb(uint16_t port, uint8_t data) { if (isa_init()) { isa_if->outp(port, data); } }
+_ISA_INL void isa_writew(uint16_t port, uint16_t data) { if (isa_init()) { isa_if->outpw(port, data); } }
+_ISA_INL uint8_t isa_readb(uint16_t port) { return isa_init() ? isa_if->inp(port) : 0xff; }
+_ISA_INL uint16_t isa_readw(uint16_t port) { return isa_init() ? isa_if->inpw(port) : 0xffff; }
+
+
+/*----------------------------------------------------------------------
+ *
+ * MS-DOS compatible functions for easy porting
+ * 
+ *--------------------------------------------------------------------*/
+#ifndef ISA_EXCLUDE_MSDOS_LIBRARY
+_ISA_INL void outp(uint16_t port, uint8_t data) { isa_writeb(port, data); }
+_ISA_INL void outpw(uint16_t port, uint16_t data) { isa_writew(port, data); }
+_ISA_INL uint8_t inp(uint16_t port) { return isa_readb(port); }
+_ISA_INL uint16_t inpw(uint16_t port) { return isa_readw(port); }
+#endif /* ISA_EXCLUDE_MSDOS_LIBRARY */
+
+#endif /* ISA_EXCLUDE_LIBRARY */
+
+#endif /* _ISA_H_ */
