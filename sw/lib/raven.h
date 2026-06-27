969f5b7599ff30e7073f0396058fa73e40549d21
agranlund <granlund23@yahoo.se>
2026-06-27 16:07:41 +0200

    a2 memory map

--- sw/lib/raven.h
+++ sw/lib/raven.h
@@ -21,8 +21,11 @@ Important:
 #define RV_PADDR_SIMM3      0x40000000UL
 #define RV_PADDR_UART1      0x20000000UL
 #define RV_PADDR_UART2      0x20000020UL
+#define RV_PADDR_DSP      	0x20000400UL		/* Rev.A2 */
+#define RV_PADDR_DSP_SLOW	0x20000440UL		/* Rev.A2 */
 #define RV_PADDR_IDE        0xA0000000UL
 #define RV_PADDR_YM         0xA1000800UL
+#define RV_PADDR_SFP       	0xA1000A40UL		/* Rev.A2 */
 #define RV_PADDR_MFP1       0xA1000A00UL
 #define RV_PADDR_MFP2       0xA0000A00UL
 #define RV_PADDR_ISA_RAM    0x80000000UL
