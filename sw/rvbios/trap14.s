0fdccb3187edd862ec39519d5a39707599609af4
agranlund <granlund23@yahoo.se>
2026-07-01 00:52:06 +0200

    rom: reverse hw polarity of ym:porta:bit6
    matching Falcon behaviour for speaker mute/unmute

--- sw/rvbios/trap14.s
+++ sw/rvbios/trap14.s
@@ -28,6 +28,10 @@
 ;		0x0016	void SetTime(u32 time)
 ;		0x0017	u32  GetTime(void)
 ;
+;		0x001C	u8 Giaccess(u16 data, u16 regno)
+;		0x001D	void Offgibit(u16 bitno)
+;		0x001E	void Ongibit(u16 bitno)
+;
 ;	TOS3
 ;		0x002E	i16  NVMAccess(i16 op, i16 start, i16 count, i8* buf)
 ;
@@ -49,6 +53,8 @@
 	.XREF 	xbc_nvmaccess
     .XREF   xbc_puntaes
     .XREF   xbc_read_temp
+
+	.XREF	xbtable
 
 	.EXPORT InstallTrap14
 
@@ -80,43 +86,29 @@ xbios_new:
 	btst	#5,(sp)			; already super?
 	beq.b	.1
 	lea		8(sp),a0		; assume longframe
-.1:	sub.l	d0,d0
+.1:	moveq	#0,d0
 	move.w	(a0),d0			; d0 = opcode
+
+	;------------------------------------
+	; Atari
+	;------------------------------------
+	cmp.w	#256,d0
+	bcc.b	.2
+	move.l	(xbtable, d0.w*4),d0
+	beq.b	.3
+	move.l	d0,a1
+	jmp		(a1)
 
 	;------------------------------------
 	; CT60
 	;------------------------------------
-	cmp.w	#0x0C6A,d0
+.2:	cmp.w	#0x0C6A,d0
 	bcc		xb_ct60
-	
-	;------------------------------------
-	; Atari
-	;------------------------------------
-IFNE XBTIME
-	cmp.w	#22,d0			; Settime
-	beq		xb_settime
-	cmp.w	#23,d0			; Gettime
-	beq		xb_gettime
-ENDIF
-
-    cmp.w   #39,d0          ; Puntaes
-    beq     xb_puntaes
-
-IFNE XBNVM
-	cmp.w	#46,d0			; NVMaccess
-	beq		xb_nvmaccess
-ENDIF
-
-	;------------------------------------
-	; CT60 / Milan
-	;------------------------------------
-	cmp.w	#160,d0			; CacheCtrl
-	beq 	xb_cache_ctrl
 
 	;------------------------------------
 	; Original
 	;------------------------------------
-	movea.l	xbios_old(pc),a0
+.3:	movea.l	xbios_old(pc),a0
 	jmp		(a0)
 
 
@@ -135,7 +127,57 @@ xb_gettime:
 	or.w	#0x0700,sr		    ; disable interrupts
 	bsr		xbc_gettime
 	rte
-	
+
+;----------------------------------------------------------
+xb_giaccess:
+	or.w	#0x0700,sr		    ; disable interrupts
+	move.w	4(a0),d0			; regno
+	move.b	d0,d1
+	and.b	#0xf,d1
+	move.b	d1,0xff8800
+	btst.b	#7,d0				; (regno & 0x80) means write
+	beq.b	.2
+	move.w	2(a0),d0			; data
+	cmp.b	#14,d1
+	bne.b	.1
+	eor.b	#0x40,d0			; portA = invert bit6
+.1:	move.b	d0,0xff8802
+.2: moveq	#0,d0
+	move.b	0xff8800,d0
+	cmp.b	#14,d1
+	bne.b	.3
+	eor.b	#0x40,d0			; portA = invert bit6
+.3: rte
+
+;----------------------------------------------------------
+xb_offgibit:
+	or.w	#0x0700,sr		    ; disable interrupts
+	move.w	2(a0),d1
+	move.b	d1,d2				; d1 = regular bits
+	eor.b	#0x40,d2			; d2 = bit6
+	and.b	#0x40,d2
+	move.b	#14,0xff8800
+	move.b	0xff8800,d0
+	and.b	d1,d0				; regular offgibit logic
+	or.b	d2,d0				; bit6 off = force 1
+	move.b	d0,0xff8802
+	rte
+
+;----------------------------------------------------------
+xb_ongibit:
+	or.w	#0x0700,sr		    ; disable interrupts
+	move.w	2(a0),d1
+	move.b	d1,d2
+	and.b	#0xbf,d1			; d1 = regular bits
+	eor.b	#0x40,d2			; d2 = bit6
+	or.b	#0xbf,d2
+	move.b	#14,0xff8800		; port a
+	move.b	0xff8800,d0
+	and.b	d2,d0				; bit6 on = force 0
+	or.b	d1,d0				; regular ongibit logic
+	move.b	d0,0xff8802
+	rte
+
 ;----------------------------------------------------------
 xb_nvmaccess:
 	or.w	#0x0700,sr		    ; disable interrupts
@@ -266,4 +308,52 @@ xb_ct60_vmalloc:
 	moveq.l	#0,d0
 	rte
 
+	
+
+xbtable:
+	dc.l	0,0,0,0,0,0,0,0,0,0	; 0
+	dc.l	0,0,0,0,0,0,0,0,0,0	; 10
+IFNE XBTIME
+	dc.l	0,0					; 20
+	dc.l	xb_settime			; 22
+	dc.l	xb_gettime			; 23
+	dc.l	0,0,0,0				; 24
+ELSE
+	dc.l	0,0,0,0,0,0,0,0
+ENDIF
+	dc.l	xb_giaccess			; 28
+	dc.l	xb_offgibit			; 29
+	dc.l	xb_ongibit			; 30
+
+	dc.l	0,0,0,0,0,0,0,0		; 31
+	dc.l	xb_puntaes			; 39
+IFNE XBNVM
+	dc.l	0,0,0,0,0,0			; 40
+	dc.l	xb_nvmaccess		; 46
+	dc.l	0,0,0				; 47
+ELSE
+	dc.l	0,0,0,0,0,0,0,0,0,0
+ENDIF
+	dc.l	0,0,0,0,0,0,0,0,0,0	; 50
+	dc.l	0,0,0,0,0,0,0,0,0,0	; 60
+	dc.l	0,0,0,0,0,0,0,0,0,0	; 70
+	dc.l	0,0,0,0,0,0,0,0,0,0	; 80
+	dc.l	0,0,0,0,0,0,0,0,0,0	; 90
+	dc.l	0,0,0,0,0,0,0,0,0,0	; 100
+	dc.l	0,0,0,0,0,0,0,0,0,0	; 110
+	dc.l	0,0,0,0,0,0,0,0,0,0	; 120
+	dc.l	0,0,0,0,0,0,0,0,0,0 ; 130
+	dc.l	0,0,0,0,0,0,0,0,0,0 ; 140
+	dc.l	0,0,0,0,0,0,0,0,0,0	; 150
+	dc.l	xb_cache_ctrl		; 160
+	dc.l	0,0,0,0,0,0,0,0,0	; 161
+	dc.l	0,0,0,0,0,0,0,0,0,0	; 170
+	dc.l	0,0,0,0,0,0,0,0,0,0	; 180
+	dc.l	0,0,0,0,0,0,0,0,0,0	; 190
+	dc.l	0,0,0,0,0,0,0,0,0,0	; 200
+	dc.l	0,0,0,0,0,0,0,0,0,0	; 210
+	dc.l	0,0,0,0,0,0,0,0,0,0	; 220
+	dc.l	0,0,0,0,0,0,0,0,0,0	; 230
+	dc.l	0,0,0,0,0,0,0,0,0,0	; 240
+	dc.l	0,0,0,0,0,0			; 250
 	
