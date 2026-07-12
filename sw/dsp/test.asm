cbb160944c8d62db826ea4d8e846e6eaf028ec94
agranlund <granlund23@yahoo.se>
2026-07-12 20:47:14 +0200

    dsp test prog

---
+++ sw/dsp/test.asm
@@ -0,0 +1,16 @@
+	include "dsp56303.inc"
+	org 	p:$0
+	jmp		start
+
+	org		p:$100
+start:
+	bset	#3,x:M_HCR				; signal HF2
+    move    #0,a                	; sawtooth counter
+loop:
+    jclr    #M_TDE,x:<<M_SSISR0,*
+    movep   a1,x:<<M_TX00       	; output left sample 
+    jclr    #M_TDE,x:<<M_SSISR0,*
+    movep   a1,x:<<M_TX00       	; output right sample
+    add     #$008000,a
+	bchg	#4,x:M_HCR				; toggle HF3
+    jmp     loop
