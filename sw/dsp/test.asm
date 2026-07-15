1748791af758a7d09eeb33f835070eea2fc1386a
agranlund <granlund23@yahoo.se>
2026-07-15 23:35:04 +0200

    Update test.asm

--- sw/dsp/test.asm
+++ sw/dsp/test.asm
@@ -6,9 +6,12 @@
 start:
 	bset	#3,x:M_HCR				; signal HF2
     move    #0,a                	; sawtooth counter
+
 loop:
     jclr    #M_TDE,x:<<M_SSISR0,*
     movep   a1,x:<<M_TX00       	; output left sample 
+	nop								; delay a bit so M_SSISR0 has
+	nop								; time to update status
     jclr    #M_TDE,x:<<M_SSISR0,*
     movep   a1,x:<<M_TX00       	; output right sample
     add     #$008000,a
