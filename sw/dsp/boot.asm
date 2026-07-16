6b2b1f6776a58e4bf5b0e421bfc39a21df18282c
agranlund <granlund23@yahoo.se>
2026-07-16 22:11:50 +0200

    Update boot.asm

--- sw/dsp/boot.asm
+++ sw/dsp/boot.asm
@@ -164,36 +164,37 @@ setup:
 	; force all essi pins to gpio as per user manual
     movep	#0,x:<<M_PCRC
 
-	; prescaler modulus 15
-	; bypass fixed prescaler
-	; divider 2
-	; 32bit word length (24bit valid data)
-    movep   #(15)|(1<<M_PSR)|(1<<12)|(4<<19),x:<<M_CRA0
+	;  0 PM[7-0]    : 15 : divide by 16
+	; 11 PSR   		:  1 : divide by eight disabled
+	; 12 DC[4-0]	:  1 : 2 words per frame
+	; 19 WL[2-0]    :  4 : 32bits, first 24bits valid
+	movep	#$20180f,x:<<M_CRA0
 
-	; M_MOD:  network mode
-	; M_SYN:  synchronous mode
-	; M_FSR:  realtive frame sync timing
-	; M_FSP:  frame sync polarity
-	; M_CKP:  clock out on falling edge, latch on rising edge
-	; M_SCKD: dsp internal clock is source
-	; M_SCD2: SC2 is output
-    movep   #(0<<M_SHFD)|(1<<M_MOD)|(1<<M_SYN)|(1<<M_FSR)|(1<<M_FSP)|(1<<M_CKP)|(1<<M_SCKD)|(1<<M_SCD2),x:<<M_CRB0
-
-	; PC0 = gpio, PC1 = pgio
-	; PC2 = SC02, PC3 = SCK0
-	; PC4 = SRD0, PC5 = STD0
-	movep	#%00111100,x:<<M_PCRC
+	; 13 MOD		:  1 : network mode
+	; 12 SYN		:  1 : synchronous mode
+	; 11 CKP		:  1 : clock polarity, falling edge
+	; 10 FSP		:  1 : frame polarity, falling edge
+	;  9 FSR		:  1 : frame sync one bit early
+	;  7 FSL[1-0]	:  0 : rx=word, tx=word
+	;  6 SHFD		:  0 : MSB shift order
+	;  5 SCKD		:  1 : DSP is clock master
+	;  4 SCD2		:  1 : SC2 is output
+	movep	#$003e30,x:<<M_CRB0
 
 	; prime transmitter
     movep	#0,x:<<M_TX00
     movep	#0,x:<<M_TX00
 
-	; enable
-	clr		a
-	movep	x:<<M_CRB0,a1
-	or		#(1<<M_SSTE0)|(1<<M_SSRE),a
-	movep	a1,x:<<M_CRB0
+	; PC0 = gpio, PC1 = pgio
+	; PC2 = SC02, PC3 = SCK0
+	; PC4 = SRD0, PC5 = STD0
+	movep	#%00111100,x:<<M_PCRC
+	movep	#%00101100,x:<<M_PRRC
 
+	; enable transmitter and receiver
+	movep	x:<<M_CRB0,a1
+	or		#$30000,a
+	movep	a1,x:<<M_CRB0
 
 	;--------------------------------------------------------------
 	; essi1
@@ -206,8 +207,8 @@ setup:
 	movep	#$000015,x:<<M_AAR1		; (sram A16) X
 	movep	#$000001,x:<<M_AAR2		; unused
 	movep	#$000001,x:<<M_AAR3		; unused
-	movep	#$012421,x:<<M_BCR		; 1 waitstate (12ns sram @ 90mhz / 112mhz)
-	;movep	#$024842,x:<<M_BCR		; 2 waitstate (15ns sram @ 90mhz / 112mhz)
+	movep	#$012421,x:<<M_BCR		; 1 waitstate (12ns sram @ 90mhz)
+	;movep	#$024842,x:<<M_BCR		; 2 waitstate (15ns sram @ 90mhz or 112mhz)
 	;movep	#$036c63,x:<<M_BCR		; 3 waitstate
 	bset	#M_APD,omr				; all AAR active
 
